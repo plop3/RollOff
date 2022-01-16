03b373b54f58fa7b5ec215ffbbbc2a578ec9724e
Serge <serge.claus@free.fr>
2022-01-16 11:47:34 +0100

    Debut intégration constantes

--- README.md
+++ README.md
@@ -12,29 +12,33 @@ Bouton à clef pour ouverture de l'abri:
     1 capteur de position du télescope (parqué: true)
     2 capteurs d'ouverture des portes
   
-  TODO:
-	- Arret de l'alimentation 12V
+TODO:
+- Eclairage de l'abri (APA106)
+- Pilotage Indi par réseau
+- Pilotage par MQTT (Home Assistant)
+- Pilotage par serveur Web
+- Park du télescope avant fermeture
+- Détection de pluie
+- Détection d'intrusion par capteur PIR
+- Alerte par SMS en cas de problème.  
+- Arret de l'alimentation 12V
   
-  OPTIONS:
-    - Barrière(s) IR de sécurité
-    - Capteurs fermeture des portes
-	- Clavier codé
+OPTIONS:
+- Barrière(s) IR de sécurité
+- Capteurs fermeture des portes
+- Clavier codé
   
 L'abri est une petite cabane sur rails.
-L'ouverture/fermeture des portes est assurée par 2 vérins électriques.
-L'ouverture/fermeture de l'abri est commandée par une motorisation de porte de garage.
-
-    Ouverture/fermeture du dome par contacteur à clef, boutons et à distance (USB, Ethernet).
-    Park du télescope et fermeture du dome en cas de pluie et au matin.
-    Alerte par SMS en cas de problème.
-    Affichage de l'heure et des informations météo.
+- L'ouverture/fermeture des portes est assurée par 2 vérins électriques.
+- L'ouverture/fermeture de l'abri est commandée par une motorisation de porte de garage.
+- Ouverture/fermeture du dome par contacteur à clef, bouton.
 
 MATERIEL
 
     Abri en bois roulant sur rails.
     2 vérins électriques pour l'ouverture/fermeture des portes.
     1 moteur de porte de garage pour le déplacement de l'abri.
-    1 accès réseau par Ethernet (W5100).
     1 carte Arduino Mega
-    1 carte 8 relais.
-    1 alimentation 220V/12V 8
+    1 carte 4 relais.
+    1 alimentation 220V/12V
+    1 convertisseur 12V/5V
--- RollOff.ino
+++ RollOff.ino
@@ -5,6 +5,92 @@
   Version 5.0
   22/10/2018-16/01/2022
 */
-  
-void setup() {}
-void loop() {}
+
+/***********/
+/* MODULES */
+/***********/
+
+/**************/
+/* PARAMETRES */
+/**************/
+#define BAUDRATE           38400       // Vitesse du port série
+#define RON  HIGH                       // Etat On pour les relais (HIGH, LOW)
+#define ROFF !RON
+#define DELAIPORTES         40000L      // Durée d'ouverture/fermeture des portes (40000L)
+#define DELAIMOTEUR         30000L      // Durée d'initialisation du moteur (40000L)
+#define DELAIABRI           22000L      // Durée de déplacement de l'abri (15000L)
+#define INTERVALLEPORTES    12000       // Intervalle entre la fermeture de la porte 1 et de la porte 2
+#define IMPMOT              500         // Durée d'impulsion moteur
+#define BAPPUILONG          3000        //Durée en ms pour un appui long sur le bouton
+
+/******************/
+/* PHERIPHERIQUES */
+/******************/
+
+/**************/
+/* CONSTANTES */
+/**************/
+//----------Sorties ----------
+#define CMDMOT      A1      // (R1) Ouverture/fermeture abri Commande moteur de porte de garage
+#define P11         3       // (R5) LM298 1 porte 1
+#define P12         5       // (R6) LM298 2 porte 1
+#define P21         6       // (R7) LM293 3 porte 2
+#define P22         7       // (R8) LM298 4 porte 2
+//---------- Entrées ----------
+// Capteurs
+#define AO          49      // Capteur abri ouvert
+#define AF          48      // Capteur abri fermé
+#define PO1         24      // Capteur porte 1 ouverte
+#define PO2         25      // Capteur porte 2 ouverte
+// Boutons
+#define BCLEF       A12     // Bouton à clef d'ouverture/fermeture 
+#define BVERT       34      // Bouton intérieur d'ouverture/fermeture   
+#define BARU        22      // Bouton d'arret d'urgence
+
+/**********/
+/* MACROS */
+/**********/
+#define CmdMotOff     digitalWrite(CMDMOT, ROFF)
+#define CmdMotOn      digitalWrite(CMDMOT, RON)
+
+/**********************/
+/* VARIABLES GLOBALES */
+/**********************/
+
+/*********/
+/* SETUP */
+/*********/
+void setup() {
+  // Connexion à AstroPi (port Indi)  
+  Serial.begin(BAUDRATE);  		
+  // Coupure de la commande du moteur de déplacement
+  CmdMotOff; pinMode(CMDMOT, OUTPUT); 
+  // Coupure des vérins de portes
+  digitalWrite(P11, LOW); pinMode(P11, OUTPUT);
+  digitalWrite(P12, LOW); pinMode(P12, OUTPUT);
+  digitalWrite(P21, LOW); pinMode(P21, OUTPUT);
+  digitalWrite(P22, LOW); pinMode(P22, OUTPUT);
+  // Activation des entrées (capteurs...)
+  pinMode(AO, INPUT_PULLUP);
+  pinMode(AF, INPUT_PULLUP);
+  pinMode(PO1, INPUT_PULLUP);
+  pinMode(PO2, INPUT_PULLUP);
+  pinMode(BCLEF, INPUT_PULLUP);
+  pinMode(BVERT, INPUT_PULLUP);
+  pinMode(BARU, INPUT_PULLUP);
+}
+
+/*********************/
+/* BOUCLE PRINCIPALE */
+/*********************/
+void loop() {
+
+}
+
+/*************/
+/* FONCTIONS */
+/*************/
+
+/************************/
+/* FONCTIONS ROLLOFFINO */
+/************************/
