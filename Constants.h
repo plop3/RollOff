f3e6cd6d8b2e7fabd7657af231468648356e671c
Plop3 <serge@plop3.fr>
2022-02-02 16:34:05 +0100

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -2,25 +2,6 @@
 /* CONSTANTES */
 /**************/
 
-//---------- RollOffIno ----------
-//  Maximum length of messages = 63                                               *|
-const char* ERROR1 = "The controller response message was too long";
-const char* ERROR2 = "The controller failure message was too long";
-const char* ERROR3 = "Command input request is too long";
-const char* ERROR4 = "Invalid command syntax, both start and end tokens missing"; 
-const char* ERROR5 = "Invalid command syntax, no start token found";
-const char* ERROR6 = "Invalid command syntax, no end token found";
-const char* ERROR7 = "Roof controller unable to parse command";
-const char* ERROR8 = "Command must map to either set a relay or get a switch";
-const char* ERROR9 = "Request not implemented in controller";
-const char* ERROR10 = "Abort command ignored, roof already stationary";
-
-const char* VERSION_ID = "V1.2-0";
-#define MAX_RESPONSE 127
-#define MAX_INPUT 45
-#define MAX_MESSAGE 63
-#define ROOF_OPEN_MILLI 120000L
-
 /**********/
 /* MACROS */
 /**********/
